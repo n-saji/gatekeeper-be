11cde41068ab5168751cb3cc8e4e10644727c994
Nikhil Saji <nikhilsaji200@gmail.com>
2026-01-14 23:50:03 -0500

    pub-sub for eda

---
+++ src/config/serverEvents.js
@@ -0,0 +1,31 @@
+const { getRedisClient } = require("./redis");
+
+let publisher;
+let subscriber;
+
+const connectPublisherSubscriber = async () => {
+  const redisClient = getRedisClient();
+  if (!redisClient || !redisClient.isOpen) {
+    await connectRedis();
+  }
+  publisher = redisClient.duplicate();
+  publisher.on("error", (err) => console.error(err));
+  await publisher.connect();
+
+  subscriber = redisClient.duplicate();
+  subscriber.on("error", (err) => console.error(err));
+  await subscriber.connect();
+
+  await subscriber.subscribe("force-logout", async (message) => {
+    const { userId } = JSON.parse(message);
+    const sessions = await redisClient.sMembers(`session:${userId}:all`);
+    for (let jti of sessions) {
+      await redisClient.del(`session:${userId}:${jti}`);
+    }
+  });
+};
+
+const getPublisher = () => publisher;
+const getSubscriber = () => subscriber;
+
+module.exports = { connectPublisherSubscriber, getPublisher, getSubscriber };
